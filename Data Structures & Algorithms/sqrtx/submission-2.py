e29f2558cb499acf71af1f2680f26967b739ea14
mehulmangave27 <mehul.mangave19@vit.edu>
2026-04-11 01:48:46 -0700

    Add: sqrtx - submission-2

---
+++ Data Structures & Algorithms/sqrtx/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        if x==1:
+            return 1
+        
+        for i in range((x//2)+1):
+            if i*i <= x and (i+1)*(i+1) > x:
+                return i
